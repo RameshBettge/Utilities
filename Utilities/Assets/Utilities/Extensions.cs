e2c7660c7a6857f05a8778588f1ac4195ff607d0
RameshBettge <bettge.r@gmx.de>
2018-08-28 14:05:51 +0200

    Created: Extensions for Vectors and Transforms. Created two folders to seperate prototyping tools and Utilities.

---
+++ Utilities/Assets/Utilities/Extensions.cs
@@ -0,0 +1,97 @@
+﻿namespace Utilities
+{
+    using UnityEngine;
+
+    public static class VectorExtensions
+    {
+        /// <summary>
+        /// Returns the Vector with one of its values set to 0.
+        /// </summary>
+        /// <param name="ignoredAxis">
+        /// The axis which should be ignored.
+        /// </param>
+        public static Vector3 Planar(this Vector3 v, Axis ignoredAxis)
+        {
+            switch (ignoredAxis)
+            {
+                case Axis.x:
+                    v.x = 0f;
+                    break;
+                case Axis.y:
+                    v.y = 0f;
+                    break;
+                case Axis.z:
+                    v.z = 0f;
+                    break;
+                default:
+                    Debug.LogWarning("Invalid Argument given for Axis: " + ignoredAxis.ToString());
+                    break;
+            }
+            return v;
+        }
+
+        /// <summary>
+        /// Creates a Vector2 using all of the values from the Vector3 given except one.
+        /// </summary>
+        /// <param name="ignoredAxis">
+        /// The axis which should be ignored.
+        /// </param>
+        /// 
+        public static Vector2 ToVector2(this Vector3 v, Axis ignoredAxis)
+        {
+            Vector2 v2 = Vector2.zero;
+
+            switch (ignoredAxis)
+            {
+                case Axis.x:
+                    v2 = new Vector2(v.y, v.z);
+                    break;
+                case Axis.y:
+                    v2 = new Vector2(v.x, v.z);
+
+                    break;
+                case Axis.z:
+                    v2 = new Vector2(v.x, v.y);
+                    break;
+                default:
+                    Debug.LogWarning("Invalid Argument given for Axis: " + ignoredAxis.ToString());
+                    break;
+            }
+            return v2;
+        }
+    }
+
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Sets the Transforms position on one axis to a given value
+        /// </summary>
+        /// <param name="axis">
+        /// The axis which is used
+        /// </param>
+        /// <param name="val">
+        /// The value to which the axis is set
+        /// </param>
+        public static void SetAxisPos(this Transform t, Axis axis, float val)
+        {
+            Vector3 p = t.position;
+
+            switch (axis)
+            {
+                case Axis.x:
+                    p = new Vector3(val, p.y, p.z);
+                    break;
+                case Axis.y:
+                    p = new Vector3(p.x, val, p.z);
+                    break;
+                case Axis.z:
+                    p = new Vector3(p.x, p.y, val);
+                    break;
+                default:
+                    Debug.LogWarning("Invalid Argument given for Axis: " + axis.ToString());
+                    break;
+            }
+            t.position = p;
+        }
+    }
+}
---
+++ Utilities/Assets/_Scripts/Tester.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Utilities;
+
+public class Tester : MonoBehaviour
+{
+    Vector3 myVector = new Vector3(1f, 2f, 3f);
+
+
+    void Start()
+    {
+        myVector = myVector.Planar(Axis.x);
+        Debug.Log(myVector);
+
+        Debug.Log(myVector.ToVector2(Axis.z));
+
+        transform.SetAxisPos(Axis.y, 8.5f);
+    }
+}
