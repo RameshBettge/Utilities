8c6d9f11d40dd9f869b189ba025f8659f43c8fbe
RameshBettge <bettge.r@gmx.de>
2018-10-25 11:59:18 +0200

    FIXED: Singleton will not create an error if something tries to access it while exiting PlayMode.
    After a singleton has been destroyed (which should only happen if application is closed) it cannot be instantiated again.

--- Utilities/Assets/Utilities/Scripts/Singleton.cs
+++ Utilities/Assets/Utilities/Scripts/Singleton.cs
@@ -1,43 +1,55 @@
-﻿namespace Utilities
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
-    using UnityEngine;
+    static T instance;
 
-    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    // is set to true as soon as the static Instance is destroyed. Used to avoid 'Some objects were not cleaned up when closing the scene'-bug
+    private static bool apllicationIsQuitting = false;
+
+    public static T Instance
     {
-        static T instance;
-
-        public static T Instance
+        get
         {
-            get
+            if (instance == null)
             {
+                if (apllicationIsQuitting)
+                {
+                    Debug.LogWarning("Preventing Instantiation of Singleton '" + typeof(T).ToString() + "', because applicationIsQuitting == true.");
+                    return null;
+                }
+
+                //If the reference isn't filled, this method will look for the corresponding GameObject in the scene. 
+                instance = (T)FindObjectOfType(typeof(T));
+
+                //A new Gameobject will be instantiated if none could be found.
                 if (instance == null)
                 {
-                    //If the reference isn't filled, this method will look for the corresponding GameObject in the scene. 
-                    instance = (T)FindObjectOfType(typeof(T));
-
-                    //A new Gameobject will be instantiated if none could be found.
-                    if (instance == null)
-                    {
-                        GameObject singletonGO = new GameObject();
-                        instance = singletonGO.AddComponent<T>();
-                        singletonGO.name = typeof(T).ToString() + " (Singleton)";
-                    }
+                    GameObject singletonGO = new GameObject();
+                    instance = singletonGO.AddComponent<T>();
+                    singletonGO.name = typeof(T).ToString() + " (Singleton)";
                 }
-                return instance;
             }
-        }
-
-        protected virtual void Awake()
-        {
-            if (instance != null && instance != this)
-            {
-                Destroy(gameObject);
-            }
-            else if (instance == null)
-            {
-                DontDestroyOnLoad(gameObject);
-                instance = (T)this;
-            }
+            return instance;
         }
     }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = (T)this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        apllicationIsQuitting = true;
+    }
 }
+
